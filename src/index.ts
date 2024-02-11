47257acf9302632600f0abb903c9338fc31794f0
svetlana-tyshkevich <panfilova.ss@gmail.com>
2024-02-11 22:08:00 +0200

    feat: add http server

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import http from 'http';
+
+const port = process.env['PORT'] || 3000;
+
+const server = http.createServer((_req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+server.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+});
