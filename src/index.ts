6434e1174c1a389d0a532c88fd21d98f4fb65737
svetlana-tyshkevich <panfilova.ss@gmail.com>
2024-02-12 18:19:54 +0200

    feat: add prettier and eslint

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,15 @@ import http from 'http';
 const port = process.env['PORT'] || 3000;
 
 const server = http.createServer((_req, res) => {
+  try {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World\n');
+  } catch {
+    res.writeHead(500, { 'Content-Type': 'text/html' });
+    res.end("Server doesn't available");
+  }
 });
 server.listen(port, () => {
-    console.log(`Server running at port ${port}`);
+  console.log(`Server running at port ${port}`);
 });
